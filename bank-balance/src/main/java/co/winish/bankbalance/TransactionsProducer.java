619e6708bb9f94591758a9dcddffa1f36fc34ef0
self-affected <ihnatov.d@gmail.com>
2020-06-03 14:08:40 +0300

    Finished bank-balance app

---
+++ bank-balance/src/main/java/co/winish/bankbalance/TransactionsProducer.java
@@ -0,0 +1,50 @@
+package co.winish.bankbalance;
+
+import co.winish.config.KafkaStreamsConfig;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TransactionsProducer {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(TransactionsProducer.class);
+
+        Producer<String, JsonNode> producer = new KafkaProducer<String, JsonNode>(KafkaStreamsConfig.getIdempotentProducerProperties());
+
+        int i = 0;
+        while (true) {
+            logger.info("Producing batch: " + i);
+            try {
+                producer.send(newRandomTransaction("Dzhamil"));
+                Thread.sleep(100);
+                producer.send(newRandomTransaction("Jean Renault"));
+                Thread.sleep(100);
+                producer.send(newRandomTransaction("Emma"));
+                Thread.sleep(100);
+
+                i++;
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+        producer.close();
+    }
+
+    public static ProducerRecord<String, JsonNode> newRandomTransaction(String username) {
+        ObjectNode transaction = JsonNodeFactory.instance.objectNode();
+
+        transaction.put("name", username);
+        transaction.put("amount", ThreadLocalRandom.current().nextInt(0, 101));
+        transaction.put("time", Instant.now().toString());
+        return new ProducerRecord<String, JsonNode>("transactions", username, transaction);
+    }
+}
--- config/src/main/java/co/winish/config/KafkaStreamsConfig.java
+++ config/src/main/java/co/winish/config/KafkaStreamsConfig.java
@@ -1,7 +1,11 @@
 package co.winish.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.*;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.StreamsConfig;
 
 import java.util.Properties;
@@ -10,6 +14,7 @@ public class KafkaStreamsConfig {
 
     public static Properties getSimpleProperties(String appId) {
         Properties properties = new Properties();
+
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, appId);
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
@@ -17,5 +22,38 @@ public class KafkaStreamsConfig {
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         return properties;
+    }
+
+    public static Properties getExactlyOnceProperties(String appId) {
+        Properties properties = new Properties();
+
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, appId);
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+        properties.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+        return properties;
+    }
+
+    public static Properties getIdempotentProducerProperties() {
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+
+        return properties;
+    }
+
+    public static Serde<JsonNode> getJsonSerde() {
+        Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+        Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+
+        return Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
     }
 }
